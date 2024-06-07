5c239657dd9e4bee39bc184ae2346095c20fb019
taka_sato <taka_sato@yumemi.co.jp>
2024-06-07 15:10:59 +0900

    Add ARCore library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.sceneview)
+    implementation(libs.arcore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
