d65ce53a47c5650cc2c24222cd88c9b49f16d22a
taka_sato <taka_sato@yumemi.co.jp>
2024-06-07 17:45:32 +0900

    Implementation: Show AR model when AugmentedImage is displayed

--- app/src/main/java/com/example/sceneviewtestapp/MainActivity.kt
+++ app/src/main/java/com/example/sceneviewtestapp/MainActivity.kt
@@ -1,43 +1,75 @@
 package com.example.sceneviewtestapp
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.sceneviewtestapp.ui.theme.SceneViewTestAppTheme
+import io.github.sceneview.ar.ARScene
+import io.github.sceneview.ar.arcore.addAugmentedImage
+import io.github.sceneview.ar.arcore.getUpdatedAugmentedImages
+import io.github.sceneview.ar.node.AugmentedImageNode
+import io.github.sceneview.math.Position
+import io.github.sceneview.node.ModelNode
+import io.github.sceneview.rememberEngine
+import io.github.sceneview.rememberModelLoader
+import io.github.sceneview.rememberNodes
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val engine = rememberEngine()
+            val modelLoader = rememberModelLoader(engine)
+            val childNodes = rememberNodes()
+            val augmentedImageNodes = mutableListOf<AugmentedImageNode>()
             SceneViewTestAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Box(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    ARScene(
+                        modifier = Modifier.fillMaxSize(),
+                        engine = engine,
+                        modelLoader = modelLoader,
+                        childNodes = childNodes,
+                        sessionConfiguration = { session, config ->
+                            config.addAugmentedImage(
+                                session,
+                                "rabbit",
+                                assets.open("augmentedimages/rabbit.jpg")
+                                    .use { BitmapFactory.decodeStream(it) }
+                            )
+                        },
+                        onSessionUpdated = { _, updatedFrame ->
+                            updatedFrame.getUpdatedAugmentedImages().forEach { augmentedImage ->
+                                if (augmentedImageNodes.none { it.imageName == augmentedImage.name }) {
+                                    val augmentedImageNode = AugmentedImageNode(engine, augmentedImage).apply {
+                                        when (augmentedImage.name) {
+                                            "rabbit" -> addChildNode(
+                                                ModelNode(
+                                                    modelInstance = modelLoader.createModelInstance(
+                                                        assetFileLocation = "models/rabbit.glb"
+                                                    ),
+                                                    scaleToUnits = 0.1f,
+                                                    centerOrigin = Position(0.0f)
+                                                )
+                                            )
+                                        }
+                                    }
+                                    childNodes.add(augmentedImageNode)
+                                    augmentedImageNodes += augmentedImageNode
+                                }
+                            }
+                        },
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SceneViewTestAppTheme {
-        Greeting("Android")
     }
 }
